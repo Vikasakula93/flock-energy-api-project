0c0ed821d37c057bfb106d78fedf547faded3c84
Vikas <vikasakula@Vikass-MacBook-Air.local>
2026-07-23 20:44:34 +0530

    Update README documentation

--- src/server.js
+++ src/server.js
@@ -12,7 +12,10 @@ const meterRoutes = require("./routes/meterRoutes");
 
 const app = express();
 
-
+app.use((req,res,next)=>{
+    console.log(req.method, req.url);
+    next();
+});
 app.use(cors());
 app.use(express.json());
 
